5e0eb19d11b2bc3e929a1c7ab7095b396981d8be
Dan Neumann <danrodneu@gmail.com>
2016-07-10 13:12:43 -0500

    Build

--- js/sprites.js
+++ js/sprites.js
@@ -1,6 +1,8 @@
 
 // 3rd
 var PIXI = require('pixi.js');
+// 1st
+var belt = require('./belt');
 
 
 //http://stackoverflow.com/questions/32078129/how-to-draw-multiple-instances-of-the-same-primitive-in-pixi-js
@@ -56,6 +58,46 @@ exports.empburstClip = function () {
   clip.anchor.set(0.5);
   clip.loop = false;
   clip.scale.set(1.50);
+  clip.play();
+  return clip;
+};
+
+
+// kind is A | B | C
+// level is 1 | 2 | 3 | 4
+exports.bombClip = function (kind, level) {
+  // Randomize if kind/level aren't set
+  kind = kind || belt.randNth(['A', 'B', 'C']);
+  level = level || Math.floor(Math.random() * 4 + 1);
+  var base = new PIXI.Texture.fromImage('./img/bombs.gif');
+  var textures = [];
+  var rowIdx = { 'A': 0, 'B': 1, 'C': 2 };
+  var offsetY = rowIdx[kind] *
+    (16 * 4) +       //  (rowHeight * levelsPerKind)
+    ((level - 1) * 16);
+  for (var i = 0; i < 10; i++) {
+    textures.push(new PIXI.Texture(base, new PIXI.Rectangle(i*16, offsetY, 16, 16)));
+  }
+  var clip = new PIXI.extras.MovieClip(textures);
+  clip.animationSpeed = 0.10;
+  clip.anchor.set(0.5);
+  clip.scale.set(1.30);
+  clip.play();
+  return clip;
+};
+
+
+exports.greenClip = function (x, y) {
+  var base = new PIXI.Texture.fromImage('./img/prizes.gif');
+  var textures = [];
+  for (var i = 0; i < 10; i++) {
+    textures.push(new PIXI.Texture(base, new PIXI.Rectangle(i*16, 0, 16, 16)));
+  }
+  var clip = new PIXI.extras.MovieClip(textures);
+  clip.animationSpeed = 0.30;
+  clip.anchor.set(0.5);
+  clip.position.set(x, y);
+  //clip.scale.set(1.30);
   clip.play();
   return clip;
 };
