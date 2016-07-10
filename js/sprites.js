1a6df85d456d30bb579daa87838805e28003ff17
Dan Neumann <danrodneu@gmail.com>
2016-07-10 10:08:14 -0500

    Build

---
+++ js/sprites.js
@@ -0,0 +1,39 @@
+
+// 3rd
+var PIXI = require('pixi.js');
+
+
+//http://stackoverflow.com/questions/32078129/how-to-draw-multiple-instances-of-the-same-primitive-in-pixi-js
+
+// Returns function that creates sprites
+exports.makeTrailMaker = function (renderer, diameter) {
+  var texture = new PIXI.RenderTexture(renderer, diameter, diameter);
+  var g = new PIXI.Graphics();
+  g.beginFill(0xF9C134);
+  g.drawCircle(diameter/2,diameter/2,diameter/2);
+  g.endFill();
+  texture.render(g);
+  return function makeTrail (x, y) {
+    var sprite = new PIXI.Sprite(texture);
+    sprite.position.set(x, y);
+    sprite.alpha = 0.50;
+    sprite.anchor.set(0.5);
+    return sprite;
+  };
+}
+
+
+exports.exhaustClip = function () {
+  var base = new PIXI.Texture.fromImage('../img/exhaust.gif');
+  var textures = [];
+  for (var i = 0; i < 19; i++) {
+    textures.push(new PIXI.Texture(base, new PIXI.Rectangle(i*16, 0, 16, 16)));
+  }
+  var clip = new PIXI.extras.MovieClip(textures);
+  clip.animationSpeed = 0.70;
+  clip.anchor.set(0.5);
+  clip.loop = false;
+  //clip.scale.set(1.30);
+  clip.play();
+  return clip;
+};
